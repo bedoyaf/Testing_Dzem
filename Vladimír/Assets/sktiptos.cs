0100b3b2625f82f2d7830eda2ed3627c3315023d
@bedoyaf <filipbedoya64@gmail.com>
2024-03-02 19:24:31 +0100

    pozmenil skriptos

--- Vladimír/Assets/sktiptos.cs
+++ Vladimír/Assets/sktiptos.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class sktiptos : MonoBehaviour
 {
     // Start is called before the first frame update
+    class sadam
+    {
+        public string name;
+    }
     void Start()
     {
-        
+        sadam nekak=new sadam();
+        nekak.name = "7";
     }
 
     // Update is called once per frame
